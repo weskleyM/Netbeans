3d81f1607e4ae1f30c42cc7bd7b985f7fd998666
weskleyM <weskley89@gmail.com>
2019-06-16 07:10:33 -0300

    Trabalho de Web

---
+++ trab_web/src/main/java/com/ufc/main/TrabWebApplication.java
@@ -0,0 +1,13 @@
+package com.ufc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabWebApplication.class, args);
+	}
+
+}
