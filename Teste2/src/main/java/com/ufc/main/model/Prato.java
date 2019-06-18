13269ec3e1cad2d4c6f62497252790e296b1110b
weskleyM <weskley89@gmail.com>
2019-06-18 17:40:30 -0300

    Trabalho de Web

--- Teste2/src/main/java/com/ufc/main/model/Prato.java
+++ Teste2/src/main/java/com/ufc/main/model/Prato.java
@@ -8,53 +8,53 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "produto")
+@Table(name = "auth_prato")
 public class Prato {
 
-     @Id
-     @GeneratedValue(strategy = GenerationType.IDENTITY)
-     @Column(name = "prod_id")
-     private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "prato_id")
+    private Long id;
 
-     @Column(name = "prod_nome")
-     private String nome;
+    @Column(name = "prato_nome")
+    private String nome;
 
-     @Column(name = "prod_desc")
-     private String desc;
+    @Column(name = "prato_desc")
+    private String desc;
 
-     @Column(name = "prod_preco")
-     private float preco;
+    @Column(name = "prato_preco")
+    private float preco;
 
-     public Long getId() {
-          return id;
-     }
+    public Long getId() {
+        return id;
+    }
 
-     public void setId(Long id) {
-          this.id = id;
-     }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-     public String getNome() {
-          return nome;
-     }
+    public String getNome() {
+        return nome;
+    }
 
-     public void setNome(String nome) {
-          this.nome = nome;
-     }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-     public String getDesc() {
-          return desc;
-     }
+    public String getDesc() {
+        return desc;
+    }
 
-     public void setDesc(String desc) {
-          this.desc = desc;
-     }
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
 
-     public float getPreco() {
-          return preco;
-     }
+    public float getPreco() {
+        return preco;
+    }
 
-     public void setPreco(float preco) {
-          this.preco = preco;
-     }
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
 
 }
