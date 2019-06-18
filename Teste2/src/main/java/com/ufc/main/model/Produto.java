f656a229eac2efdd5a18a823a3f5534bd1a7ba81
weskleyM <weskley89@gmail.com>
2019-06-18 13:59:25 -0300

    Trabalho de Web

---
+++ Teste2/src/main/java/com/ufc/main/model/Produto.java
@@ -0,0 +1,60 @@
+package com.ufc.main.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     @Column(name = "prod_id")
+     private Long id;
+
+     @Column(name = "prod_nome")
+     private String nome;
+
+     @Column(name = "prod_desc")
+     private String desc;
+
+     @Column(name = "prod_preco")
+     private float preco;
+
+     public Long getId() {
+          return id;
+     }
+
+     public void setId(Long id) {
+          this.id = id;
+     }
+
+     public String getNome() {
+          return nome;
+     }
+
+     public void setNome(String nome) {
+          this.nome = nome;
+     }
+
+     public String getDesc() {
+          return desc;
+     }
+
+     public void setDesc(String desc) {
+          this.desc = desc;
+     }
+
+     public float getPreco() {
+          return preco;
+     }
+
+     public void setPreco(float preco) {
+          this.preco = preco;
+     }
+
+}
