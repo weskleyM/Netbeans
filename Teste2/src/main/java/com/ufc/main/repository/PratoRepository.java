c591f8531fce36771a22de6d18272dc1e3a63794
weskleyM <weskley512@gmail.com>
2019-07-05 09:57:38 -0300

    updated

--- Teste2/src/main/java/com/ufc/main/repository/PratoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ufc.main.repository;
-
-import com.ufc.main.model.Prato;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PratoRepository extends JpaRepository<Prato, Long> {
-
-}
